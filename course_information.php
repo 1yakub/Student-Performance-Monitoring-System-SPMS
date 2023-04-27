e77164703a4fcdda53b5a798c9b1176ca44101f7
Md. Yakub Hossain <1830968@iub.edu.bd>
2023-04-26 19:55:38 -0700

    Add files via upload

---
+++ course_information.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>View Students</title>
+	<link rel="stylesheet" href="style_style.css">
+
+
+</head>
+
+<body>
+	<h1>View Students</h1>
+	<form method="post" action="course_information.php">
+
+
+		<label for="course_id">course id:</label>
+
+		<input type="text" id="year" name="year">
+		<label for="year">Educational year:</label>
+		<input type="text" id="year" name="year">
+
+		<label for="semester">Educational semester:</label>
+		<input type="text" id="semester" name="semester">
+
+		<label for="courseID">Enrolled Course:</label>
+		<input type="text" id="courseID" name="courseID">
+
+		<label for="sectionID">Enrolled Section:</label>
+		<input type="text" id="sectionID" name="sectionID">
+
+		<input type="submit" value="View Students">
+	</form>
+</body>
+
+</html>
